28578235dbb8a63d25116e8f923b0d3bc0de3789
Hoon Kim <hoonsubin@icloud.com>
2020-08-16 21:37:10 -0700

    refactored functions

--- src/helpers/pullRequestEvent.ts
+++ src/helpers/pullRequestEvent.ts
@@ -1,22 +1,10 @@
-import { Context } from 'probot';
+import { Context, Octokit } from 'probot';
 import { PullRequestCode } from 'src/types';
 import fileExts from '../data/filesToCheck.json';
 import commentTemplate from 'src/data/commentTemplate';
 import { prChangesToBullet } from './markdownConverter';
 
-export const handlePullRequest = async (context: Context) => {
-    const pr = context.payload.pull_request;
-    if (!pr || pr.state !== 'open') return;
-
-    const org = pr.base.repo.owner.login;
-    const repo = pr.base.repo.name;
-
-    const files = await context.github.pulls.listFiles({
-        number: pr.number,
-        owner: org,
-        repo: repo,
-    });
-
+const prFilesToFormat = (files: Octokit.Response<Octokit.PullsListFilesResponse>) => {
     const searchRegex = `^.*\.(${fileExts.join('|')})$`;
 
     const changedFiles = files.data.filter((i) => {
@@ -50,10 +38,32 @@ export const handlePullRequest = async (context: Context) => {
         }
     });
 
+    return {
+        addedFiles,
+        moddedFiles,
+        removedFiles,
+    };
+};
+
+export const handlePullRequest = async (context: Context) => {
+    const pr = context.payload.pull_request;
+    if (!pr || pr.state !== 'open') return;
+
+    const org = pr.base.repo.owner.login;
+    const repo = pr.base.repo.name;
+
+    const files = await context.github.pulls.listFiles({
+        number: pr.number,
+        owner: org,
+        repo: repo,
+    });
+
+    const _changes = prFilesToFormat(files);
+
     const commentBody = commentTemplate(
-        prChangesToBullet(addedFiles),
-        prChangesToBullet(moddedFiles),
-        prChangesToBullet(removedFiles),
+        prChangesToBullet(_changes.addedFiles),
+        prChangesToBullet(_changes.moddedFiles),
+        prChangesToBullet(_changes.removedFiles),
     );
 
     const pull = context.issue();

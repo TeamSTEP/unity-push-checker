0a374409fb78748b850882581bd4167e5de797fb
Hoon Kim <hoonsubin@icloud.com>
2020-12-13 05:01:40 -0800

    updated octokit response types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Application } from 'probot';
+import { ApplicationFunctionOptions } from 'probot/lib/types';
 import * as Controllers from './controllers';
 
-export = async (app: Application) => {
+export default ({ app }: ApplicationFunctionOptions) => {
     app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'], async (context) => {
         try {
             const pr = context.payload.pull_request;
